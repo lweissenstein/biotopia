f4527ddb17ce9d716640e78d32859b430f2a4778
yumimeko <131466542+yumimekoo@users.noreply.github.com>
2025-04-20 01:31:57 +0200

    implemented grid-feature + adding/destroying objects + simple camera movement

---
+++ development/project_urban/Assets/Scripts/Grid System/ObjectPlacer.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    [SerializeField] System.Collections.Generic.List<GameObject> placedGameObject = new();
+
+    public int PlaceObject(GameObject prefab, Vector3 position)
+    {
+        GameObject newObject = Instantiate(prefab);
+        newObject.transform.position = position;
+        placedGameObject.Add(newObject);
+        return placedGameObject.Count - 1;
+    }
+
+    internal void RemoveObjectAt(int gameObjectIndex)
+    {
+        if (placedGameObject.Count <= gameObjectIndex || placedGameObject[gameObjectIndex] == null)
+            return;
+        Destroy(placedGameObject[gameObjectIndex]);
+        placedGameObject[gameObjectIndex] = null;
+    }
+}
